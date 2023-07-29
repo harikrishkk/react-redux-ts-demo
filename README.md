260ffc1a29cd9985d844f4635d80b3623bda7d3b
Hari <harikrishkk@gmail.com>
2023-07-29 08:55:11 -0400

    commit 3: wire up store

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```
 npm create vite@latest react-redux-ts-demo --template react-ts
 npm install @reduxjs/toolkit
+npm i react-redux
 ```
---
+++ src/app/features/counter/counterSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface CounterState {
+  value: number;
+}
+
+const INIT_STATE: CounterState = {
+  value: 10
+}
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: INIT_STATE,
+  reducers: {
+    increment: (state) => {
+      // we get intelliscence here
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    }
+  }
+})
+
+export default counterSlice.reducer;
